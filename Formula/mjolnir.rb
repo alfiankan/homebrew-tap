7f716fa5783bf0ff3e52b725411d0f6c0e46aff6
alfiankan <alfiankan19@gmail.com>
2022-03-11 00:03:22 +0700

    Update mjolnir.rb

--- Formula/mjolnir.rb
+++ Formula/mjolnir.rb
@@ -1,8 +1,8 @@
 class Mjolnir < Formula
   desc "Chained database inspired from block chain, for medical records or ownership records or other data supported"
   homepage "https://github.com/alfiankan/mjolnir"
-  url "https://github.com/alfiankan/mjolnir/releases/download/v1.0.0/mjolnir-mac.tar.gz"
-  sha256 "379d586ec048413b41c9a3419e80f338bad3a962b3eeb61848c9f391448e7a69"
+  url "https://github.com/alfiankan/mjolnir/releases/download/v1.0.1/mjolnir-mac.tar.gz"
+  sha256 "35aca63e8dcd0bc4abf12176373b04ed354674aeca345076c78226aaa8d47444"
   version "1.0.0"
 
   
