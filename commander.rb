c35f3e34a73405bcb3fdb19e40d83fb72695a52c
alfiankan <alfiankan19@gmail.com>
2022-12-20 16:30:35 +0700

    Update commander.rb

--- commander.rb
+++ commander.rb
@@ -5,19 +5,19 @@ class Commander < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/alfiankan/commander/releases/download/v0.2.1/cmdr-v0.2.1-darwin-amd64.tar.gz"
-    sha256 "127a253cbf7b7b8c74018f074f0452aabb0bb29270e63b34782a951fa46f8487"
+    sha256 "b5578741aaa05eb15c36cf15281dda528993649508cc27bf44c2552d32c7cd62"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/alfiankan/commander/releases/download/v0.2.1/cmdr-v0.2.1-darwin-arm64.tar.gz"
-    sha256 "d7f3fc2a1986efa0e9f116d11b1a6e5fae11d2aaa71428593681e63cfbe65113"
+    sha256 "60a0da527d766f72dd336c461b1e534788de1615a4685ad8095e23305073d20f"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/alfiankan/commander/releases/download/v0.2.1/cmdr-v0.2.1-linux-amd64.tar.gz"
-    sha256 "97bf347820972cdc247126382be095cf87d99ca4c38391a1c35e897cbcf95297"
+    sha256 "0e661ac7bdea38673112609ac8db6c3e28e9847bae09f35ce9d14881a6002e5c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/alfiankan/commander/releases/download/v0.2.1/cmdr-v0.2.1-linux-arm64.tar.gz"
-    sha256 "4511e587ce86fced027152391551c38f6feea5a9afb2e3cbe845f465d96b9cb0"
+    sha256 "bdbb55ad5e9d8e4cd9e5ed9b9d13c5bf9f6d16daab3acd2c65a160e08d83b4d1"
   end
 
   def install
